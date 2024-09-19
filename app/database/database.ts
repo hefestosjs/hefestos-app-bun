d875d7d34094c9d54c5fdf030b96fe4287844d20
Lucas Nathan J. da Silva <lucasnathanj@gmail.com>
2024-09-19 17:09:35 -0300

    fix: add the bind function to keep prisma this

--- app/database/database.ts
+++ app/database/database.ts
@@ -1,9 +1,9 @@
 import { prisma } from ".";
 
 export const Database = {
-  transaction: prisma.$transaction,
-  queryRaw: prisma.$queryRaw,
-  queryRawUnsafe: prisma.$queryRawUnsafe,
-  executeRaw: prisma.$executeRaw,
-  executeRawUnsafe: prisma.$executeRawUnsafe,
+  transaction: prisma.$transaction.bind(prisma),
+  queryRaw: prisma.$queryRaw.bind(prisma),
+  queryRawUnsafe: prisma.$queryRawUnsafe.bind(prisma),
+  executeRaw: prisma.$executeRaw.bind(prisma),
+  executeRawUnsafe: prisma.$executeRawUnsafe.bind(prisma),
 };
